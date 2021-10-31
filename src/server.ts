4fd931fe4f285180d93d2ddc9bd6ff38d0c2d5af
Jarred Beverly <jbevely@aesintl.com>
2021-10-31 10:08:17 -0400

    created filterimage endpoint that accepts image url and sends the file back

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { Request, Response } from "express"
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+
+type ImageReqQuery = {
+  image_url: string;
+}
 
 (async () => {
 
@@ -13,23 +19,29 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
 
-  //! END @TODO1
+  app.get('/filteredimage', async (req: Request<any, any, any, ImageReqQuery>, res) => {
+    if(!req.query.image_url) {
+      res.status(400).send({response: 'query parameter image_url required.'})
+    }
+
+    try {
+      const localPath = await filterImageFromURL(req.query.image_url);
+      res.sendFile(localPath, {}, err => {
+        if(err) {
+          throw new Error('File send error')
+        }
+        // clean up files if any.
+        deleteLocalFiles([localPath]);
+      })
+
+    }
+
+    catch (e) {
+      res.status(500).send({response: 'unable to filter image from provided URL'})
+    }
+  })
   
   // Root Endpoint
   // Displays a simple message to the user
